1f6798268a9e91eddba7222f010b449d0009c0e3
Sean Dever <Seand0611@gmail.com>
2019-10-01 18:45:08 -0700

    updating our app to redicrect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
@@ -17,7 +17,7 @@ class App extends React.Component {
   //firebase notifies app that the auth state has changed(user updated)
   //sends the user and then it gets called
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -39,6 +39,7 @@ class App extends React.Component {
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
   render() {
     return (
       <div>
@@ -46,15 +47,29 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route 
+            exact
+            path='/signin'
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            } 
+          />
         </Switch>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
 })
 
-export default connect(null, mapDispatchToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/UserReducer.js
+++ src/redux/user/UserReducer.js
@@ -1,10 +1,12 @@
+import { SET_CURRENT_USER } from './UserTypes';
+
 const initalState = {
     currentUser: null
 }
 
 const userReducer = (state=initalState, action) => {
     switch(action.type) {
-        case 'SET_CURRENT_USER':
+        case SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
